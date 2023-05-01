3ecf2e370437620548d3ecab2146158ae36fc14a
IERoldan <ivanroldan1989@gmail.com>
2023-05-01 12:25:08 -0300

    start responsive adaptation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const arrayCodes = ['e','i','a','o','u'];
-const arrayDecodes = ['enter','imes','ai','ober','ufat'];
+const arrayCodes = ['e', 'i', 'a', 'o', 'u'];
+const arrayDecodes = ['enter', 'imes', 'ai', 'ober', 'ufat'];
 
 const TextOfInput = document.getElementById("text-input").value;
 let containerTextEncrypted = document.getElementById('container-text-encrypted').innerHTML = `
@@ -10,45 +10,48 @@ let containerTextEncrypted = document.getElementById('container-text-encrypted')
                 <h3>Ningún mensaje fue encontrado</h3>
                 <p class="a-2-parr">Ingresa el texto que desea encriptar o desencriptar</p>
             </div>
-
-
             <div class="a-1" id="a-1">
                 <p class="text-reveal" id="text-reveal"></p>
             </div>
 
 `
 
-function textToWork(X,Y) {
-    let containerTextEncrypted = document.getElementById('container-text-encrypted').innerHTML = `
+function textToWork(X, Y) {
+    let containerTextEncrypted = document.getElementById('container-text-encrypted');
+    containerTextEncrypted.innerHTML = `
     <div class="a-1" id="a-1">
                     <p class="text-reveal" id="text-reveal"></p>
                 </div>
                 <div class="a-2 tooltip">
-                    <button class="btn-3" onclick="copyText()" onmouseout="outFunc()"> <span class="tooltiptext" id="myTooltip" >Copiar al portapapeles</span>Copiar<lord-icon src="https://cdn.lordicon.com/pqxdilfs.json" trigger="hover"
+                    <button class="btn-3" onclick="copyTextToClipboard()" onmouseout="updateTooltipText()">
+                    <span class="tooltiptext" id="myTooltip" >
+                    Copiar al portapapeles
+                    </span>
+                    Copiar
+                    <lord-icon src="https://cdn.lordicon.com/pqxdilfs.json" trigger="hover"
                             colors="primary:#121331,secondary:#646e78,tertiary:#ffc738,quaternary:#f9c9c0,quinary:#ebe6ef"
                             style="width:30px;height:30px" class='icon-copy'>
-                        </lord-icon>
+                    </lord-icon>
                     </button>
                 </div>
     
     `
-    for(var position = 0; position <= X.length ; position++){
-    const textToAnalize = document.getElementById("text-input").value;
-    document.getElementById("text-input").value = textToAnalize.replaceAll(X[position], Y[position]);
-    document.getElementById("text-reveal").innerHTML = textToAnalize;
-}
-document.getElementById("text-input").value = TextOfInput;
+    for (let position = 0; position <= X.length; position++) {
+        const textToAnalize = document.getElementById("text-input").value;
+        document.getElementById("text-input").value = textToAnalize.replaceAll(X[position], Y[position]);
+        document.getElementById("text-reveal").innerHTML = textToAnalize;
+    }
+    document.getElementById("text-input").value = TextOfInput;
 };
 
-function copyText(){
-    var textToCopy = document.getElementById('text-reveal').innerHTML;
+function copyTextToClipboard() {
+    const textToCopy = document.getElementById('text-reveal').textContent;
     navigator.clipboard.writeText(textToCopy);
-    // alert(`Texto copiado ${textToCopy}` );
-    var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Texto Copiado: " + textToCopy;
+    const tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Texto Copiado: " + textToCopy;
 }
 
-function outFunc() {
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copiar al portapapeles";
-  }
+
+function updateTooltipText() {
+    const tooltip = document.getElementById("myTooltip").innerHTML;
+}
