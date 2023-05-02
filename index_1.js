76a0258cd96d66b526afdf0bcb0e03210c46f998
IERoldan <ivanroldan1989@gmail.com>
2023-05-02 18:55:38 -0300

    new aside

---
+++ index_1.js
@@ -0,0 +1,13 @@
+const arrayCodes = ['e', 'i', 'a', 'o', 'u'];
+const arrayDecodes = ['enter', 'imes', 'ai', 'ober', 'ufat'];
+
+const TextOfInput = document.getElementById("text-input").value;
+// let containerTextEncrypted = document.getElementById('container-text-encrypted').innerHTML = `
+// <div class="a-1">
+//             <img src="/assets/muñeco.svg" class="muneco" alt="muñeco">
+//           </div>
+//           <div class="a-2">
+//             <h3 class="a-2-text">Ningún mensaje fue encontrado</h3>
+//                 <p class="a-2-text">Ingresa el texto que desea encriptar o desencriptar</p>
+//           </div>
+// `
